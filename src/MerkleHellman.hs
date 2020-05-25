ebaf6b0610921cb7f8a8feb93b969ffe8275ae1e
wovik <wovik77@gmail.com>
2020-05-25 22:52:13 +0200

    arbitrary legth key generator

--- src/MerkleHellman.hs
+++ src/MerkleHellman.hs
@@ -13,35 +13,34 @@ type PrivKey = (Integer, Integer, [Integer])
 type Message = [Integer]
 type Cryptogram = Integer
 
-generatePrivateKey :: StdGen -> PrivKey
-generatePrivateKey g = (m, w, rods)
+generatePrivateKey :: StdGen -> Integer -> PrivKey
+generatePrivateKey g n = (m, w, rods)
   where
-    (m , newGen )    = randomR (2 ^ 201 + 1, 2 ^ 202 - 1) g
+    (m , newGen )    = randomR (2 ^ (2 * n + 1) + 1, 2 ^ (2 * (n + 1)) - 1) g
     (w', newGen')    = randomR (2, m - 2) g
     w                = w' `div` gcd m w'
-    (rods, newGen'') = generateRods newGen' 100
+    (rods, newGen'') = generateRods newGen' n n
 
 
-generateRods :: StdGen -> Integer -> ([Integer], StdGen)
-generateRods g 0 = ([], g)
-generateRods g i = (restA ++ [a], gen)
+generateRods :: StdGen -> Integer -> Integer -> ([Integer], StdGen)
+generateRods g 0 n = ([], g)
+generateRods g i n = (restA ++ [a], gen)
   where
-    (restA, gen   ) = generateRods g (i - 1)
-    (a    , newGen) = generateA gen i
+    (restA, gen   ) = generateRods g (i - 1) n
+    (a    , newGen) = generateA gen i n
 
-generateA :: StdGen -> Integer -> (Integer, StdGen)
-generateA g i =
-    randomR ((2 ^ (i - 1) - 1) * 2 ^ 100, (2 ^ (i - 1)) * 2 ^ 100) g
+generateA :: StdGen -> Integer -> Integer -> (Integer, StdGen)
+generateA g i n = randomR ((2 ^ (i - 1) - 1) * 2 ^ n, (2 ^ (i - 1)) * 2 ^ n) g
 
 generatePublicKey :: PrivKey -> PubKey
 generatePublicKey (_, _, []) = []
 generatePublicKey (m, w, (a : as)) =
     (w * a `mod` m) : generatePublicKey (m, w, as)
 
-generate :: StdGen -> KeyPair
-generate g = (priv, pub)
+generate :: StdGen -> Integer -> KeyPair
+generate g n = (priv, pub)
   where
-    priv = generatePrivateKey g
+    priv = generatePrivateKey g n
     pub  = generatePublicKey priv
 
 encrypt :: PubKey -> Message -> Cryptogram
@@ -52,6 +51,7 @@ eGCD :: Integer -> Integer -> (Integer, Integer, Integer)
 eGCD 0 b = (b, 0, 1)
 eGCD a b = let (g, s, t) = eGCD (b `mod` a) a in (g, t - (b `div` a) * s, s)
 
+decryptImpl :: Cryptogram -> [Integer] -> Message
 decryptImpl s []       = []
 decryptImpl s (a : as) = decryptImpl s1 as ++ [msg]
     where (msg, s1) = if s >= a then (1, s - a) else (0, s)
--- test/MerkleHellmanSpec.hs
+++ test/MerkleHellmanSpec.hs
@@ -3,13 +3,19 @@ module MerkleHellmanSpec
     )
 where
 
-import           Test.Hspec
 import           MerkleHellman
+import           System.Random
+import           Test.Hspec
 
 pubKey = [5457, 1663, 216, 6013, 7439]
 privKey = (8443, 2550, [171, 196, 457, 1191, 2410])
 msg = [0, 1, 0, 1, 1]
 crypt = 15115
+
+increasingSum _ [] = True
+increasingSum s (a:as)
+    | s < a = increasingSum (s+a) as
+    | s >= a = False
 
 spec :: Spec
 spec = do
@@ -19,3 +25,25 @@ spec = do
     describe "decrypting" $ do
         it "should decrypt" $ do
             decrypt privKey crypt `shouldBe` msg
+    let g = mkStdGen 2 
+        keyPair = generate g 5
+        ((m, w, as'), as) =  keyPair in
+        describe "generating small numbers" $ do
+            it "should generate small keys" $ do
+                keyPair `shouldBe` ((3612,1781,[21,53,117,245,501]),[1281,481,2493,2905,117])
+            it "should retrun w and m with gcd == 1" $ do
+                gcd m w `shouldBe` 1
+            it "should keep increasing value of as'" $ do
+                as' `shouldSatisfy` (increasingSum 0)
+    let g = mkStdGen 3
+        keyPair = generate g 100
+        ((m, w, as'), as) =  keyPair in
+        describe ("generating large numbers with seed " ++ show g) $ do
+            it "should retrun w and m with gcd == 1" $ do
+                gcd m w `shouldBe` 1
+            it "should keep increasing value of as'" $ do
+                as' `shouldSatisfy` (increasingSum 0)
+            it "should make private key of length 100"$ do
+                length as' `shouldBe` 100
+            it "should make public key of length 100"$ do
+                length as `shouldBe` 100
